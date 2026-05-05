d0f94bc3345bc61de1e6c029d9bb66bd6d7f2396
raice <ricenoob@toke.com>
2026-05-04 21:21:48 -0300

    refactor: argument parsing

--- Program.cs
+++ Program.cs
@@ -10,64 +10,92 @@ namespace STR {
             return input.Equals(full, StringComparison.OrdinalIgnoreCase);
         }
 
-        static void Main(string[] args) {
-            var cmd = args.FirstOrDefault() ?? "";
-            if (args.Length > 0) {
-                if (args[0].ToLower() == "--compile") {
-                    if (args.Length > 1) {
-                        Compiler.Compile(args[1]);
+        static void ParseArgs(string[] args) {
+            foreach (string arg in args)
+            {
+                switch (arg.ToLower())
+                {
+                    case "--compile":
+                    {
+                        if (args.Length != 2) {
+                            Console.WriteLine("No .asm file supplied for compiling,\nUse: --compile <file.str>");
+                            break;
+                        }
+
+                        var file = args[1];
+
+                        if (!File.Exists(file)) {
+                            Console.WriteLine("The file does not exist!");
+                            break;
+                        }
+
+                        // just my opinion, but better not check for .S files,
+                        // not common with 8086 assembly.
+                        if (!file.ToLower().EndsWith(".asm")) {
+                            Console.WriteLine("This isn\'t a .asm file! Rename it.");
+                            break;
+                        }
+                        
+                        Compiler.Compile(file);
+                        break;
                     }
-                } else {
-                    var bytes = File.ReadAllBytes(args[0]);
-                    for (int i = 0; i < bytes.Length; i++) { // load the code on the memory
-                        CPU.mem[i] = bytes[i];
-                        Console.WriteLine(bytes[i]);
+                    case "--help":
+                    {
+                        Console.WriteLine("str - stupidly simple cpu emulator");
+                        Console.WriteLine();
+
+                        Console.WriteLine("Usage:");
+                        Console.WriteLine("  str --compile <file.asm>    Compile an ASM file into a STR binary");
+                        Console.WriteLine("  str <file.str>              Run a STR program");
+                        Console.WriteLine();
+
+                        Console.WriteLine("Options:");
+                        Console.WriteLine("  --help                      Show this help message");
+                        Console.WriteLine("  --version                   Show version information");
+
+                        break;
                     }
 
-                    CPU.Update();
+                    case "--version":
+                    {
+                        Console.WriteLine($"str version {versionMajor}.{versionMinor}");
+                        Console.WriteLine("a simple cpu emulator written in c#");
+                        Console.WriteLine();
+                        Console.WriteLine("made by nixxlte and raice");
+                        break;
+                    }
+
+                    default:
+                    {
+                        if (arg.ToLower().EndsWith(".str")) {
+                            if (File.Exists(arg)) {
+                                var bytes = File.ReadAllBytes(arg);
+                                for (int i = 0; i < bytes.Length; i++) { // load the code on the memory
+                                    CPU.mem[i] = bytes[i];
+                                    Console.WriteLine(bytes[i]);
+                                }
+
+                                CPU.Initialize();
+                            }
+                        } else {
+                            Console.WriteLine($"Unknown argument: {arg}");
+                        }
+                        break;
+                    }
                 }
-            } else {
-                Console.WriteLine("Please enter a file to run in STR cpu");
-                Thread.Sleep(5000);
             }
-            
+        }
+
+        static void Main(string[] args) {
+            ParseArgs(args);
+
+            // HACK: quick hack for checking if user opened via consolew
+            // im NOT proud of this :cry:
+            var (left, top) = Console.GetCursorPosition();
+            if (left==0 && top==0)
+            {
+                Thread.Sleep(3000); // waiting 3 seconds for user to read in case of errors
+            }
         }
     }
 }
-
-// little program to:
-// ax = 0, bx = 5
-// while ax != 5, ax++
-
-// AX = 0
-//CPU.mem[0] = 1;
-//CPU.mem[1] = CPU.AX; 8
-//CPU.mem[2] = 7;
-
-// BX = 5
-//CPU.mem[3] = 1;
-//CPU.mem[4] = CPU.BX; 9
-//CPU.mem[5] = 5;
-
-// CMP AX, BX
-//CPU.mem[6] = 5;
-//CPU.mem[7] = CPU.AX; 8 
-//CPU.mem[8] = CPU.BX; 9
-
-// JZ -> HALT
-//CPU.mem[9] = 4;
-//CPU.mem[10] = 16;
-
-// AX = AX + 1
-//CPU.mem[11] = 2;
-//CPU.mem[12] = CPU.AX;
-//CPU.mem[13] = 1;
-
-// JMP loop
-//CPU.mem[14] = 3;
-//CPU.mem[15] = 6;
-
-// HALT
-//CPU.mem[16] = 99;
-
-//RUN.INT(10);
